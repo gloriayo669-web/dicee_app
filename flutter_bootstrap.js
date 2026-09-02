503e2c6bd541a49c9259fd2a0a66c5131bdfdc3c
Glory Ayodele <gloriayo669@gmail.com>
2026-09-02 15:42:01 +0100

    Built web
    Branch: main
    Commit: 10374af57d42e8a9f7acd2a85457bb52ea19d3fa
    
    package:peanut 6.0.1

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"83675ed27633283e7fc296c8bca22e841224c0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3330843311" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1445449005" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
